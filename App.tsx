c423d496e32cedfc7c5d62dc0a5dfabaeb5acbb0
irsyad <irsyadk4@gmail.com>
2024-12-06 20:37:55 +0700

    changed to RNreanimated

--- App.tsx
+++ App.tsx
@@ -9,16 +9,21 @@ import {
   Text,
   useColorScheme,
   View,
-  Animated,
-  TouchableOpacity
+  Animated as Animat ,
+  TouchableOpacity,
+  Button
 } from 'react-native';
 
 import {
   Colors,
 } from 'react-native/Libraries/NewAppScreen';
-import { SharedValue } from 'react-native-reanimated';
+import Animated, { Extrapolation, interpolate, SharedValue, useAnimatedScrollHandler, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
 const { width, height } = Dimensions.get('screen');
+const _itemwidth = width* 0.7
+const _itemheight = _itemwidth* 1.5
+const _spacing = 20 
+const _itemfullsize = _itemwidth + _spacing
 const IMG_WIDTH = width * 0.75;
 const IMG_HEIGHT = IMG_WIDTH * 1.45;
 
@@ -31,11 +36,8 @@ interface IData {
 interface ICard {
   index : number
   data : IData
-  translateX: any
-  translateY: any
-  scale: any
   active : boolean
-  scaleAnim: Animated.Value;
+  scrollx : SharedValue<number>
   handlePressItem: (index : number) => void
 }
 
@@ -47,28 +49,33 @@ export interface CardRenderProps {
   marginHorizontal?: DimensionValue;
 }
 
-const Card = ( {data, active, translateX,translateY, scale, index, scaleAnim , handlePressItem} : ICard) => {
-  const cardScaleX = scaleAnim.interpolate({
-    inputRange: [0, 1],
-    outputRange: [1, 1.3], 
-    extrapolate: 'clamp',
-  });
+const Card = ( {data, active, index , handlePressItem,scrollx} : ICard) => {
+  const widthAnimated = useSharedValue(_itemwidth);
+  const heigthAnimated = useSharedValue(_itemheight);
+  const [activetemp, setActivetemp] = useState(false)    
 
-  const cardScaleY = scaleAnim.interpolate({
-    inputRange: [0, 1],
-    outputRange: [1, 1.5], 
-    extrapolate: 'clamp',
-  });
+  const stylez = useAnimatedStyle(() => {
+    return {
+      transform: [{scale :interpolate(scrollx.value,[
+        index-10, index , index +10
+      ],[-0.8,1,0,8],Extrapolation.CLAMP)},{translateX :interpolate(activetemp? 1 : 0,[
+        index-1, index , index +1
+      ],[-5,1,5],Extrapolation.CLAMP)}]
+    }
+  })
 
   return (
     <TouchableOpacity onPress={() => {
       handlePressItem(index)
+      widthAnimated.value = withSpring(activetemp == false ? width : _itemwidth);
+      heigthAnimated.value = withSpring(activetemp == false ? height : _itemheight);
+      setActivetemp(!activetemp)
       }}>
-      <Animated.View style={[{
-            width: IMG_WIDTH,
-            height: IMG_HEIGHT,
-            transform: [{ translateX }, { translateY }, { scale }, { scaleX: cardScaleX }, { scaleY: cardScaleY }],
+      <Animated.View style={[stylez,{
+            width: widthAnimated,
+            height: heigthAnimated,
             backgroundColor: 'white',
+            borderRadius:8,
           }, styles.shadow]}>
         <Text style={{textAlign:'center'}}>{data.title}</Text>
         <Image width={60} height={60} source={{uri : data.image}}/>
@@ -81,31 +88,23 @@ const Card = ( {data, active, translateX,translateY, scale, index, scaleAnim , h
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const scrollx = useSharedValue(0)
+  const onScroll = useAnimatedScrollHandler((event) => {
+    scrollx.value = event.contentOffset.x / _itemfullsize
+  })
+
   const [indexActive, setIndexActive] = useState<number | null>(null)
 
-  const [scaleAnims, setScaleAnims] = useState<Animated.Value[]>([]); 
 
     const handlePressItem = (index: number) => {
     if (indexActive === index) {
-      Animated.spring(scaleAnims[index], {
-        toValue: 0, 
-        friction: 3,
-        tension: 150,
-        useNativeDriver: true,
-      }).start();
+     
       setIndexActive(null); 
     } else {
-      Animated.spring(scaleAnims[index], {
-        toValue: 1, 
-        friction: 3,
-        tension: 150,
-        useNativeDriver: true, 
-      }).start();
+     
       setIndexActive(index); 
     }
   };
-
-  const scrollX = useRef(new Animated.Value(0)).current
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
@@ -118,71 +117,35 @@ function App(): React.JSX.Element {
     {title:'lorem ipsum', content:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum', image:'https://picsum.photos/200/300'},
    ]
 
-  // Initialize scale animations for each card
-  const initializeScaleAnims = () => {
-    const scaleArray = data.map(() => new Animated.Value(0)); // One scale value per card
-    setScaleAnims(scaleArray);
-  };
-
-    // Initialize the scale animations when the component mounts
-    React.useEffect(() => {
-      initializeScaleAnims();
-    }, []);
-
   return (
     <SafeAreaView style={[backgroundStyle, {flex:1}]}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
         backgroundColor={backgroundStyle.backgroundColor}
       />
+     
       <View style={{flex:1}}>
-      <Animated.FlatList
-        data={data}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        pagingEnabled
-        style={{flex:1}}
-        contentContainerStyle={{paddingVertical:20}}
-        keyExtractor={(item,index) => item.title + index}
-        renderItem={({item, index}) => {
-          const inputRange = [
-            (index - 1) * width,
-            index * width,
-            (index + 1) * width,
-          ]
+        <Animated.FlatList
+          data={data}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+          pagingEnabled
+          contentContainerStyle={{gap: _spacing, paddingHorizontal : (width - _itemwidth)/2}}
+          snapToInterval={_itemfullsize}
+          decelerationRate={'fast'}
+          keyExtractor={(item,index) => item.title + index}
+          onScroll={onScroll}
+          scrollEnabled={indexActive == null}
 
-          // Interpolate scale value
-          const scale = scrollX.interpolate({
-            inputRange,
-            outputRange: [0.8, 1, 0.8], // Scale down unfocused items
-            extrapolate: 'clamp'
-          });
-
-
-          const translateX = scrollX.interpolate({
-            inputRange,
-            outputRange: [-width * .3, 0, width * .3]
-          })
-
-          const translateY = scrollX.interpolate({
-            inputRange ,
-            outputRange: [0, 0, 0]
-          })
-
-          const scaleAnim = scaleAnims[index] || new Animated.Value(0);
-
-          
-          return (
-          <View style={{width, justifyContent: "center", alignItems: "center", zIndex : index}}> 
-              <View style={{width: IMG_WIDTH, borderRadius: 15, height: IMG_HEIGHT, alignItems: "center", }}>
-                <Card  handlePressItem={handlePressItem}  scaleAnim={ scaleAnim} index={index} scale={scale} translateX={translateX} translateY={translateY} active={indexActive == index} data={item}/>
-              </View>
-          </View>
-          )
-        }}
-        onScroll={Animated.event(
-          [{nativeEvent: {contentOffset: {x: scrollX}}}],
-          {useNativeDriver: true},)}
+          renderItem={({item, index}) => {
+            return (
+            <View style={{alignItems: "center", marginHorizontal: indexActive == index ? 10 : 0}}> 
+                <View style={{width: _itemwidth, borderRadius: 15, height: _itemheight, alignItems: "center", }}>
+                  <Card scrollx={scrollx}  handlePressItem={handlePressItem}  index={index} active={indexActive == index} data={item}/>
+                </View>
+            </View>
+            )
+          }}
         />
       </View>
       </SafeAreaView>
